b4b0553cf492277383b900f23a05f4894837157c
Lukas Mitro <lukas.mitro@globallogic.com>
2026-04-14 14:26:58 +0200

    Remove dist folder from git

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    hmr: {
+      host: 'localhost',
+      port: 3000
+    },
+    watch: {
+      usePolling: true
+    }
   },
   preview: {
     host: '0.0.0.0',
